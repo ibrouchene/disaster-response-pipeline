54a89c724456ea8caacf29229cdb8bf318b51d3e
Ibrouchene <ibrouchene.nassim@gmail.com>
2021-02-23 18:56:02 +0100

    new file names

--- data/process_data.py
+++ data/process_data.py
@@ -1,6 +1,5 @@
 import sys
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine
 
 
@@ -10,7 +9,7 @@ def load_data(messages_filepath, categories_filepath):
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     # Merge input data
-    df = pd.concat([messages, categories], axis=1)  # outer join is by default done    
+    df = pd.concat([messages, categories], axis=1)  # outer join is by default done
     return df
 
 
@@ -46,20 +45,29 @@ def save_data(df, database_filename):
 
 
 def main():
+    if len(sys.argv) == 4:
 
-    messages_filepath, categories_filepath, database_filepath = r".\data\messages.csv", r".\data\categories.csv", r".\data\CleanDataBase.db"
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-    print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-          .format(messages_filepath, categories_filepath))
-    df = load_data(messages_filepath, categories_filepath)
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
 
-    print('Cleaning data...')
-    df = clean_data(df)
+        print('Cleaning data...')
+        df = clean_data(df)
 
-    print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-    #save_data(df, database_filepath)
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
 
-    print('Cleaned data saved to database!')
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
 
 
 if __name__ == '__main__':
